2b3753a487a290197bf8833b941d530881847f2b
Kevin <kffh.vanleeuwen@student.avans.nl>
2019-01-20 11:22:17 +0100

    het werkt nu? 2

--- src/main/java/kvl/school/ivh11/cnf/WebMVCConfig.java
+++ src/main/java/kvl/school/ivh11/cnf/WebMVCConfig.java
@@ -1,18 +1,21 @@
 package kvl.school.ivh11.cnf;
 
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.ui.context.ThemeSource;
 import org.springframework.ui.context.support.ResourceBundleThemeSource;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ThemeResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.theme.CookieThemeResolver;
 import org.springframework.web.servlet.theme.ThemeChangeInterceptor;
+
+import java.util.Locale;
 
 @Configuration
 @EnableWebMvc
@@ -36,13 +39,24 @@ public class WebMVCConfig extends WebMvcConfigurerAdapter
         resolver.setDefaultThemeName("pulse");
         return resolver;
     }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+        return sessionLocaleResolver;
+    }
+    @Bean
+    public LocaleChangeInterceptor languageLocator() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
     @Override
-    public void addInterceptors(InterceptorRegistry registry)
-    {
+    public void addInterceptors(InterceptorRegistry registry) {
         //interationalization
-        LocaleChangeInterceptor languageLocator = new LocaleChangeInterceptor();
-        languageLocator.setParamName("Dutch");
-        registry.addInterceptor(languageLocator);
+        registry.addInterceptor(languageLocator());
 
         //theming
         ThemeChangeInterceptor themeChangeInterceptor = new ThemeChangeInterceptor();
--- src/main/java/kvl/school/ivh11/web/controller/MovieController.java
+++ src/main/java/kvl/school/ivh11/web/controller/MovieController.java
@@ -2,12 +2,11 @@ package kvl.school.ivh11.web.controller;
 
 import kvl.school.ivh11.dto.FilmDTO;
 import kvl.school.ivh11.service.abstr.FilmService;
+import kvl.school.ivh11.components.Messages;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDateTime;
@@ -19,8 +18,6 @@ import java.util.Set;
 public class MovieController
 {
     private FilmService filmService;
-    private Locale locale;
-    private MessageSource messageSource;
 
     @Autowired
     public MovieController(FilmService fs)
@@ -29,19 +26,12 @@ public class MovieController
     }
 
     @RequestMapping(value="/all", method = RequestMethod.GET)
-    public ModelAndView getFilms(@RequestParam(name = "lang") String lang)
-    {
+    public ModelAndView getFilms() {
         LocalDateTime ld = LocalDateTime.now();
         Set<FilmDTO> films = filmService.getFilmsPlayingToday(ld);
 
-        if(lang.length() > 0)
-            this.locale = Locale.forLanguageTag(lang);
-        else
-            this.locale = Locale.ENGLISH;
-
         ModelAndView filmVM = new ModelAndView("movies/overview");
         filmVM.addObject("films", films);
-        filmVM.addObject("welcomeTxt", messageSource.getMessage("movies.overviewPageTxt", null, locale));
 
         return filmVM;
     }
